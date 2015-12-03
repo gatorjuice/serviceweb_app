ea10753366551af4e150ce7c7e054f4f07010c19
Jamie <gatorjuice@gmail.com>
2015-12-03 11:12:44 -0600

    fixed scaling on forms for mobile viewing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202000416) do
+ActiveRecord::Schema.define(version: 20151202235646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20151202000416) do
     t.string   "name"
     t.string   "address"
     t.string   "phone"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "city"
